ac5a2e58c25bd1162836a9f6edc23c413082931a
Burak Han Demirbilek <burakhan.demirbilek@gmail.com>
2022-03-14 18:58:10 +0300

    Updated setup.py and .gitignore

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+# Quadsim: A Quadcopter Rotational Dynamics
+# Simulation For Reinforcement Learning Algorithms
+# To install, run the command below:
+# python setup.py install --user
+
 from setuptools import setup, find_packages
+
 setup(
     name='quadsim',
     packages=find_packages(),
